b0606306e2cc0915191ecbbcf152ce690878ea99
James J Balamuta <james.balamuta@gmail.com>
2016-03-03 18:25:42 -0600

    First commit of SITMO

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sitmo
-Sitmo parallel random number engine headers in an R package
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+[![Travis-CI Build Status](https://travis-ci.org/coatless/sitmo.svg?branch=master)](https://travis-ci.org/coatless/sitmo)
+
+SITMO for R
+===========
+
+The repository provides an example of the SITMO R package for Parallel Randomized Numbers via OpenMP.
---
+++ src/sitmo_test.cpp
@@ -0,0 +1,81 @@
+#include <RcppArmadillo.h>
+#include <sitmo.h>
+
+// [[Rcpp::export]]
+arma::vec qexp( arma::vec& u, double lambda ){
+  
+  u = arma::log(0.1e1 - u);
+  u = - u / lambda;
+  
+  return u;
+}
+
+double qexp( double u, double lambda ){
+  
+  u = std::log(0.1e1 - u);
+  u = - u / lambda;
+  
+  return u;
+}
+
+
+// Simulate samples
+
+// // [[Rcpp::export]]
+// arma::vec rexp( unsigned int n, double lambda, const unsigned int& seed ){
+//   
+//   arma::vec u(n);
+//   
+//   // Generate a vector of uniform random variables
+//   // std::srand(seed);
+//   std::mt19937_64 engine(seed);  // Mersenne twister random number engine
+//   std::uniform_real_distribution<double> distr(0.0, 1.0);
+//   u.imbue( [&]() { return distr(engine); } );
+//   
+//   // Transform into Dagum random variables
+//   qexp(u,lambda);
+//   
+//   return u;
+// }
+
+// [[Rcpp::export]]
+arma::vec test_sitmo(unsigned int n, double lambda, arma::vec seeds){ 
+  
+  unsigned int ncores = seeds.n_elem;
+
+  arma::vec q(n);
+  
+  #ifdef _OPENMP
+  #pragma omp parallel num_threads(ncores) if(ncores > 1)
+  {
+  #endif
+  
+    // Engine requires uint32_t inplace of unsigned int
+    uint32_t active_seed;
+      
+    // Write the active seed per core or just write one of the seeds.
+    #ifdef _OPENMP
+      active_seed = static_cast<uint32_t>(seeds(omp_get_thread_num()));
+    #else
+      active_seed = static_cast<uint32_t>(seeds(0));
+    #endif
+    
+    sitmo::prng_engine engine( active_seed );
+    std::uniform_real_distribution<double> distunif(0.0, 1.0);
+  
+  
+    // Parallelize the Loop
+    #ifdef _OPENMP
+    #pragma omp for schedule(static)
+    #endif
+    for (unsigned int i = 0; i < n; i++){
+      double u = distunif(engine);
+      q(i) = qexp(u,lambda);
+    }
+
+  #ifdef _OPENMP
+  }
+  #endif
+  
+  return q;
+}
